11267685de5a30f4f5c104ed9fc5ed81c4ece158
K Vamsi <99885183+vamsi8106@users.noreply.github.com>
2024-07-10 14:29:59 +0530

    Update utils.py

--- Logistic_Regression/utils.py
+++ Logistic_Regression/utils.py
@@ -1,9 +1,27 @@
 import numpy as np
 
 def sigmoid(x):
-    x = np.clip(x, -88.72, 88.72)
-    return (1/(1 + np.exp(-x)))
+    """
+    Computing the sigmoid function for input x.
+
+    Parameters:
+    x (numpy.ndarray): Input array.
+
+    Returns:
+    numpy.ndarray : Sigmoid of x.
+    """
+    x = np.clip(x, -88.72, 88.72)  # Clip x to prevent overflow in exponential function for float32 precision
+    return 1 / (1 + np.exp(-x))  # Sigmoid function: 1 / (1 + e^(-x))
 
 def accuracy(y_pred, y):
+    """
+    Computimg accuracy of predictions compared to true labels.
 
-    return np.sum(y_pred == y) / len(y)
+    Parameters:
+    y_pred (numpy.ndarray): Predicted labels.
+    y (numpy.ndarray): True labels.
+
+    Returns:
+    float: Accuracy as a ratio of correct predictions to total predictions.
+    """
+    return np.sum(y_pred == y) / len(y)  # Compute accuracy as the ratio of correct predictions to total predictions
